eb0f08843084885ed186997adf199233013f48f0
justas- <justas.poderys@gmail.com>
2017-01-09 15:21:54 +0100

    fix naming and remove class

--- PyALTO-node/PyALTO_node.py
+++ PyALTO-node/PyALTO_node.py
@@ -7,12 +7,12 @@ import json
 import socket
 import ptvsd
 
-import NetStatsCollector
+import nethelpers
 
 # Enable remote execution from the Visual Studio
 ptvsd.enable_attach(secret='alto')
 
-def main(run_arg):
+def main(run_args):
     """Main entry point for collector"""
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
     logging.info("PyALTO NODE is starting")
@@ -20,15 +20,13 @@ def main(run_arg):
     if sys.platform != 'linux' and sys.platform != 'linux2':
         logging.error('Collector supports Linux only!')
         return
-
-    collector = NetStatsCollector.NetStatsCollector()
     
     ## Event loop
     loop = asyncio.get_event_loop()
     loop.set_debug(True)
 
     # Setup call to data reporting
-    loop.call_later(15.0, lambda: report_stats(run_arg, collector))
+    loop.call_later(run_args['collect-interval'], report_stats, run_args)
 
     # Run until terminated
     try:
@@ -39,25 +37,26 @@ def main(run_arg):
     # If we are asked to close - close the connection and end the loop
     loop.close()
 
-def report_stats(run_arg, collector):
+def report_stats(run_args):
     """Periodically report stats"""
     logging.info('Hostname: {}'.format(socket.gethostname()))
     logging.info('Adapters:')
-    logging.info(collector._get_net_adapter_names())
+    logging.info(nethelpers.get_net_adapter_names())
     logging.info('Stats:')
-    logging.info(collector.collect_all_interface_stats())
+    logging.info(nethelpers.collect_all_interface_stats())
     logging.info('Rtable:')
-    logging.info(collector.get_routing_table())
+    logging.info(nethelpers.get_routing_table())
 
     loop = asyncio.get_event_loop()
-    loop.call_later(15.0, lambda: report_stats(run_arg, collector))
+    loop.call_later(run_args['collect-interval'], report_stats, run_args)
     
 if __name__ == "__main__":
     # Parse the command line arguments
     parser = argparse.ArgumentParser(description='ALTO Virtual Net device stats collector')
 
     parser.add_argument('--alto-server', help='IP Address of the ALTO server', nargs='?', default='192.168.100.100')
-    parser.add_argument('--dev-type', help='Type of Virtual device (switch/router)', nargs='?', default='router')
+    parser.add_argument('--dev-type', help='Type of the virtual device (switch/router)', nargs='?', default='router')
+    parser.add_argument('--collect-interval', help='Data collection frequency', nargs='?', default=15.0)
     
     args = parser.parse_args()
     
