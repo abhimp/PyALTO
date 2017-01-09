f079641653f174ff0739bdd99768adb9f0ee9caf
justas- <justas.poderys@gmail.com>
2017-01-09 10:12:04 +0100

    Update Node code to use asyncio

--- PyALTO-node/PyALTO_node.py
+++ PyALTO-node/PyALTO_node.py
@@ -1,103 +1,18 @@
 import logging
 import asyncio
+import argparse
 import os
-import ipaddress
-import binascii
 import sys
 import json
-import datetime
+import socket
 import ptvsd
-import argparse
 
 import NetStatsCollector
 
 # Enable remote execution from the Visual Studio
 ptvsd.enable_attach(secret='alto')
 
-# Create a TCP connection to the ALTO server
-def GetIfaceStats(ifaces):
-    """Get stats of each iface"""
-    stats = {}
-    for iface in ifaces:
-        stats_dict = {}
-        stat_dir = os.path.join(os.path.join('/sys/class/net', iface), 'statistics')
-        for stat in os.listdir(stat_dir):
-            stat_path = os.path.join(stat_dir, stat)
-            fp = open(stat_path, 'r')
-            stats_dict[stat] = int(fp.read())
-        stats[iface] = stats_dict
-
-    return stats
-
-
-def GetRoutingDetails():
-    """Get kernel routing table"""
-    rt = open('/proc/net/route', 'r')
-    
-    rtable = []
-    lnnum = 0
-    while True:
-        # Skip line with headings
-        ln = rt.readline()
-        if lnnum == 0:
-            #print(ln)
-            lnnum = lnnum + 1
-            continue
-
-        # Read lines until the end
-        if ln == None or ln == '':
-            break
-        else:
-            lnnum = lnnum + 1
-
-        item = 0
-        line = {}
-
-        # Parse each column
-        for word in ln.split('\t'):
-            if item == 0:
-                line['iface'] = word
-            elif item == 1:
-                addr = int.from_bytes(binascii.unhexlify(word), sys.byteorder)
-                ip = ipaddress.IPv4Address(addr)
-                line['destination'] = ip
-            elif item == 2:
-                addr = int.from_bytes(binascii.unhexlify(word), sys.byteorder)
-                ip = ipaddress.IPv4Address(addr)
-                line['gateway'] = ip
-            elif item == 3:
-                int_fl = int(word)
-                arr_fl = []
-                if int_fl & 0b00000001:
-                    arr_fl.extend('U')
-                if int_fl & 0b00000010:
-                    arr_fl.extend('G')
-                line['flags'] = arr_fl
-            elif item == 4:
-                line['refcnt'] = word
-            elif item == 5:
-                line['use'] = word
-            elif item == 6:
-                line['metric'] = int(word)
-            elif item == 7:
-                addr = int.from_bytes(binascii.unhexlify(word), sys.byteorder)
-                ip = ipaddress.IPv4Address(addr)
-                line['mask'] = ip
-            elif item == 8:
-                line['mtu'] = int(word)
-            elif item == 9:
-                line['window'] = int(word)        
-            elif item == 10:
-                line['irtt'] = int(word.strip())
-            item = item + 1
-
-        # add line to the table
-        rtable.append(line)
-
-    # Return the table
-    return rtable
-
-def main(argv):
+def main(run_arg):
     """Main entry point for collector"""
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
     logging.info("PyALTO NODE is starting")
@@ -106,12 +21,16 @@ def main(argv):
         logging.error('Collector supports Linux only!')
         return
 
-    nsc = NetStatsCollector.NetStatsCollector()
-
+    collector = NetStatsCollector.NetStatsCollector()
+    
     ## Event loop
     loop = asyncio.get_event_loop()
     loop.set_debug(True)
 
+    # Setup call to data reporting
+    loop.call_later(15.0, lambda: report_stats(run_arg, collector))
+
+    # Run until terminated
     try:
         loop.run_forever()
     except KeyboardInterrupt:
@@ -120,6 +39,19 @@ def main(argv):
     # If we are asked to close - close the connection and end the loop
     loop.close()
 
+def report_stats(run_arg, collector):
+    """Periodically report stats"""
+    logging.info('Hostname: {}'.format(socket.gethostname()))
+    logging.info('Adapters:')
+    logging.info(collector._get_net_adapter_names())
+    logging.info('Stats:')
+    logging.info(collector.collect_all_interface_stats())
+    logging.info('Rtable:')
+    logging.info(collector.get_routing_table())
+
+    loop = asyncio.get_event_loop()
+    loop.call_later(15.0, lambda: report_stats(run_arg, collector))
+    
 if __name__ == "__main__":
     # Parse the command line arguments
     parser = argparse.ArgumentParser(description='ALTO Virtual Net device stats collector')
