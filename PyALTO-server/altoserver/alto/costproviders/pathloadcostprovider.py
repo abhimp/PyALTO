eea5f92386ae6dcd78e3499861541a9bc6fa18a7
justas- <justas.poderys@gmail.com>
2017-09-12 13:20:15 +0200

    Added LGPL blob

--- PyALTO-server/altoserver/alto/costproviders/pathloadcostprovider.py
+++ PyALTO-server/altoserver/alto/costproviders/pathloadcostprovider.py
@@ -1,4 +1,22 @@
 """
+PyALTO, a Python3 implementation of Application Layer Traffic Optimization protocol
+Copyright (C) 2016,2017  J. Poderys, Technical University of Denmark
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+"""
 Routing cost provider that is using residual path capacity as cost metric.
 To calculate residual path load, obtain path from source to destinatio.
 If path exists, it will contain one ot more segments (links between devices).
